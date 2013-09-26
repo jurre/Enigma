456ea0523306b6caf4de014b57d0ef6dfdf8a923
Jurre Stender <jurrestender@hotmail.com>
2013-09-26 23:32:27 +0200

    improve usage message

--- enigma.rb
+++ enigma.rb
@@ -1,7 +1,7 @@
 require_relative 'lib/enigma_machine'
 
 # print usage and terminate if no input is given
-abort "Usage: ruby enigma_machine <sentence to encrypt/decrypt>" if ARGV.size == 0
+abort "Usage: ruby enigma.rb <sentence to encrypt/decrypt>" if ARGV.size == 0
 
 input = ARGV.join(' ')
 
